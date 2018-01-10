684f930911392b8d43b9a332e316ab36bd452e37
BKI <b.kisfali@gmail.com>
2018-01-10 18:10:21 +0100

    initial commit

---
+++ server/api.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const apiRouter = express.Router();
+
+const minions = require('./db.js');
+
+//routes for api/minons
+apiRouter.get('/minions', (req, res, next) => {
+  res.send(minions.getAllFromDatabase('minions'));
+});
+
+apiRouter.get('/minions/:minionId', (req, res, next) => {
+  const id = req.params.minionId;
+  const index = minions.getFromDatabaseById('minions', id);
+  if (index === -1 || isNaN(id) || id >= 450) {
+    res.status(404).send();
+  } else {
+    res.status(200).send(index);
+  }
+});
+
+apiRouter.put('/minions/:minionId', (req, res, next) => {
+  const id = req.params.minionId;
+  const instance = minions.getFromDatabaseById('minions', id);
+  if (id >= 450 || instance === -1 || isNaN(id) ) {
+    res.status(404).send();
+  } else {
+    minions.updateInstanceInDatabase('minions', req.query);
+    res.send(minions.db.allMinions.data.id);
+  }
+})
+
+module.exports = apiRouter;
